f33c93f1d212492699a20f7b0483d6727683068a
skiingwiz <skiingwiz@gmail.com>
2016-11-15 22:31:00 -0500

    scripts updates

--- src/scripts/resources/run.bat
+++ src/scripts/resources/run.bat
@@ -3,7 +3,7 @@ setlocal
 
 cd /D "%~dp0"
 
-java -Dlogback.configurationFile=./logback.groovy -jar @jar.name@ -c conf/playon.properties %*
+java -Dlogback.configurationFile=./logback.groovy -jar @jar.name@ -c conf/base.properties %*
 
 pushd ..\sage-scripting-framework
 java -jar ssf.jar scripts\scanLibrary.py
--- src/scripts/resources/run_manual.bat
+++
@@ -1,3 +0,0 @@
-cd /D "%~dp0"
-java -Dlogback.configurationFile=./logback.groovy -jar @jar.name@ --no-fanart %*
-
